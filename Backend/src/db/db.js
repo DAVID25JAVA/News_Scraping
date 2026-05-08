57b7a31a48d2973cfcd58b4281f31eef6601ee44
David <dp2072000@gmail.com>
2026-05-08 18:40:38 +0530

    Database connection done

---
+++ Backend/src/db/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const ConnectDB = async (req, res) => {
+  try {
+    const connect = await mongoose.connect(
+      `${process?.env?.MONGODB_URI}/News-Scrape`
+    );
+    console.log("Database connected successfully", connect.connection.host);
+  } catch (error) {
+    console.log("Database connection error--->", error?.message);
+    process.exit(1);
+  }
+};
+
+export default ConnectDB;
